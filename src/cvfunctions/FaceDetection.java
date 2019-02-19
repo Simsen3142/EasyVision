eac838c7197e2afe323c522230345ae96e90ce5d
Simsen3142 <Simon.Senoner@outlook.com>
2019-02-19 19:16:30 +0100

    VideoStreamer removed from FaceDetection

--- src/cvfunctions/FaceDetection.java
+++ src/cvfunctions/FaceDetection.java
@@ -15,10 +15,9 @@ import org.opencv.videoio.VideoCapture;
 // used sample: https://opencv-java-tutorials.readthedocs.io/en/latest/06-face-detection-and-tracking.html
 // https://github.com/opencv-java/face-detection
 // https://github.com/opencv-java/face-detection/blob/master/src/it/polito/teaching/cv/FaceDetectionController.java
-public class FaceDetection extends MatEditFunction{
+public class FaceDetection extends MatEditFunction {
 	private int absoluteFaceSize=0;
-	private CascadeClassifier faceCascade = new CascadeClassifier();
-	private VideoCapture capture = new VideoCapture();
+	private transient CascadeClassifier faceCascade = new CascadeClassifier();
 	
 	// public methods
 	// Haar
@@ -27,46 +26,46 @@ public class FaceDetection extends MatEditFunction{
 	}
 	
 	public Mat detectEyesHaar(Mat frame) {
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		String xmlResource = loader.getResource("LaneDetection/src/resources/haarcascade_eye.xml").getPath();
-		File file = new File(xmlResource);
-		xmlResource = file.getAbsolutePath();
+//		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+//		String xmlResource = loader.getResource("LaneDetection/src/resources/haarcascade_eye.xml").getPath();
+		File file = new File("LaneDetection/src/resources/haarcascade_eye.xml");
+		String xmlResource = file.getAbsolutePath();
 		CascadeClassifier faceDetector = new CascadeClassifier(xmlResource);
 		System.out.println(xmlResource);
 		return startDetection(xmlResource,frame);
 	}
 	
 	public Mat detectEyesWithGlassesHaar() {
-		return startDetection("/LaneDetection/src/resources/haarcascade_eye_tree_eyeglasses.xml");
+		return startDetection("LaneDetection/src/resources/haarcascade_eye_tree_eyeglasses.xml");
 	}
 	
 	public Mat detectFrontalFaceHaar() {
-		return startDetection("/LaneDetection/src/resources/haarcascade_frontalface_alt.xml");
+		return startDetection("LaneDetection/src/resources/haarcascade_frontalface_alt.xml");
 	}
 	
 	public Mat detectFullBodyHaar() {
-		return startDetection("/LaneDetection/src/resources/haarcascade_fullbody.xml");
+		return startDetection("LaneDetection/src/resources/haarcascade_fullbody.xml");
 	}
 	
 	public Mat detectUpperBodyHaar() {
-		return startDetection("/LaneDetection/src/resources/haarcascade_upperbody.xml");
+		return startDetection("LaneDetection/src/resources/haarcascade_upperbody.xml");
 	}
 	
 	public Mat detectSmilingFaceHaar() {
-		return startDetection("/LaneDetection/src/resources/haarcascade_smile.xml");
+		return startDetection("LaneDetection/src/resources/haarcascade_smile.xml");
 	}
 	
 	public Mat detectProfileFaceHaar() {
-		return startDetection("/LaneDetection/src/resources/haarcascade_profileface.xml");
+		return startDetection("LaneDetection/src/resources/haarcascade_profileface.xml");
 	}
 	
 	// LBP
 	public Mat detectFrontalFaceLBP() {
-		return startDetection("/LaneDetection/src/resources/lbpcascade_frontalface_improved.xml");
+		return startDetection("LaneDetection/src/resources/lbpcascade_frontalface_improved.xml");
 	}
 	 
 	public Mat detectProfileFaceLBP() {
-		return startDetection("/LaneDetection/src/resource/lbpcascade_profileface.xml");
+		return startDetection("LaneDetection/src/resource/lbpcascade_profileface.xml");
 	}
 	
 	//private methods
@@ -74,52 +73,20 @@ public class FaceDetection extends MatEditFunction{
 	private Mat startDetection(String classifierPath) { // classifierPath is the path to where the resource is located
 		Mat frame = new Mat();
 				
-		loadClassifier(classifierPath);
-		if (capture.isOpened())
-		{
-			try
-			{
-				capture.read(frame);
-				if (!frame.empty())
-				{
-					detectAndDisplay(frame);
-				}
-						
-			}
-			catch (Exception e)
-			{
-				System.err.println("Exception during the image elaboration: " + e);
-			}
-		}
+		detectAndDisplay(frame);
 		
 		return frame;
 	}
 	
 	private Mat startDetection(String classifierPath, Mat frame) { // classifierPath is the path to where the resource is located
 		loadClassifier(classifierPath);
-		if (capture.isOpened())
-		{
-			try
-			{
-				capture.read(frame);
-				if (!frame.empty())
-				{
-					detectAndDisplay(frame);
-				}
-						
-			}
-			catch (Exception e)
-			{
-				System.err.println("Exception during the image elaboration: " + e);
-			}
-		}
+		detectAndDisplay(frame);
 		
 		return frame;
 	}
 	    
 	private void loadClassifier(String classifierPath) {
         faceCascade.load(classifierPath);
-        capture.open(0);
 	}
 	
 	private void detectAndDisplay(Mat frame)
